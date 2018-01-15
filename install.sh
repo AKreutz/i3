580b98b01ac49b6f3c443849f3fb057786c90e3e
Andreas Kreutz <andreas.kreutz@hotmail.de>
2018-01-15 22:42:04 +0100

    Add arc-themes to the install script.

--- install.sh
+++ install.sh
@@ -5,5 +5,18 @@ apt-get install compton
 apt-get install feh
 apt-get install scrot
 apt-get install thunar
+apt-get install lxappearance
+apt-get install arc-theme
+apt-get install autoconf
+apt-get install ffmpeg
 
 cp wallpaper/* ~/Bilder/
+
+mkdir ~/tmp
+
+cd ~/tmp
+git clone https://github.com/horst3180/arc-icon-theme --depth=1 && cd arc-icon-theme
+./autogen.sh --prefix=/usr
+sudo make install
+
+# select icons + arc-darker theme using lxappearance
