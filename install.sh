27af446ac66ed02b6f9e57b6aea8e8a84bc5e9b6
Andreas Kreutz <andreas.kreutz@hotmail.de>
2018-01-16 14:12:38 +0100

    Add missing program installations

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-apt-get install fonts-font-awesome
-apt-get install compton
-apt-get install feh
-apt-get install scrot
-apt-get install thunar
-apt-get install lxappearance
-apt-get install arc-theme
-apt-get install autoconf
-apt-get install ffmpeg
+# Spotify
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
+apt-get update
+
+apt-get install spotify-client fonts-font-awesome compton feh xbacklight scrot acpi thunar lxappearance arc-theme autoconf ffmpeg
 
 cp wallpaper/* ~/Bilder/
 
@@ -18,5 +15,4 @@ cd ~/tmp
 git clone https://github.com/horst3180/arc-icon-theme --depth=1 && cd arc-icon-theme
 ./autogen.sh --prefix=/usr
 sudo make install
-
 # select icons + arc-darker theme using lxappearance
