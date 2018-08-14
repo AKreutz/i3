a79f12f6dd31122903ee260e7702b6bc555b2551
Andreas Kreutz <andreas.kreutz@hotmail.de>
2018-08-15 01:47:11 +0200

    Add light for backlight control

--- install.sh
+++ install.sh
@@ -7,12 +7,18 @@ apt-get update
 
 apt-get install spotify-client fonts-font-awesome compton feh xbacklight scrot acpi thunar lxappearance arc-theme autoconf ffmpeg
 
-cp wallpaper/* ~/Bilder/
+cp wallpaper/* ~/Pictures/
 
 mkdir ~/tmp
 
 cd ~/tmp
 git clone https://github.com/horst3180/arc-icon-theme --depth=1 && cd arc-icon-theme
 ./autogen.sh --prefix=/usr
+sudo make install
+
+curl https://github.com/haikarainen/light/archive/1.1.2.tar.gz -o ../tmp/light.tar.gz
+cd ../tmp && tar xf light.tar.gz
+cd light-x.yy
+make
 sudo make install
 # select icons + arc-darker theme using lxappearance
